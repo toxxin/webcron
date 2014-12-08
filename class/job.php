445607af554c20dccfda1593850bdc7746ae1fea
Anton Glukhov <toxxinus@gmail.com>
2014-12-08 14:31:19 +0300

    Add base parsing to human readable format

--- class/job.php
+++ class/job.php
@@ -49,6 +49,36 @@ class Job
 		$this->script_file = $s;
 	}
 	
+		
+	private function parse($part)
+	{
+	    if ($part == '*') {
+	        return 'any';
+	    }
+	    if (substr($part, 0, 2) == '*/') {
+	        return 'every ' . $this->parse(substr($part, 2)); /* XXX: recursion */
+	    }
+	    preg_match("/\D/is", $part, $list, PREG_OFFSET_CAPTURE);
+
+	    $index = $list[0][1];
+	    if ($index) {
+	        $return = substr($part, 0, $index);
+	        $nextPart = substr($part, $index);
+	        switch ($nextPart[0]) {
+	            case '-':
+	                $return .= ' until ';
+	                break;
+	            case ',':
+	                $return .= ' and ';
+	                break;
+	        }
+	        $return .= $this->parse(substr($nextPart, 1)); /* XXX: recursion */
+	    } else {
+	        $return = $part;
+	    }
+	    return $return;
+	}
+
 	
     	public function toEnglish()
     	{
@@ -58,8 +88,8 @@ class Job
 	            'At hour    : ' . $this->parse($hour) . '<br>' .
 	            'At day     : ' . $this->parse($day) . '<br>' .
 	            'At month   : ' . $this->parse($month) . '<br>' .
-	            'At weekday : ' . $this->parse($weekday) . '<br>' .
-	            'At year    : ' . $this->parse($year ?: '*') . '<br>'
+	            'At weekday : ' . $this->parse($weekday) . '<br>' 
+	            //'At year    : ' . $this->parse($year ?: '*') . '<br>'
 	            ;
     	}
 
